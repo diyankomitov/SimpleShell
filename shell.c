689f566087169314614a6aa7e2257fa9693a2716
Gordon Clark <ntb15144@uni.strath.ac.uk>
2017-02-02 10:47:23 +0000

    console input removed some bad stuff

--- shell.c
+++ shell.c
@@ -4,27 +4,21 @@
 #define MEM_SIZE 32
 #define STR_LEN 512
 
-void read_from_console(char mem[][STR_LEN]);
+void read_from_console();
 void main(int argc, char * argv[]){
-	int i;
-	char mem[MEM_SIZE][STR_LEN];
-	read_from_console(mem);
+	read_from_console();
 	return;	
 }
 
-void read_from_console(char mem[][STR_LEN]){
-
+void read_from_console(){
 	char input[STR_LEN] = {""};
-	int count = 0;
-	int i = 0;
-	for (i = 0; i < MEM_SIZE; i++){ 
-	    strcpy(mem[i], "00000000");
-	}
-	while(strcmp(input,"exit\n") != 0){
+	while(1){
 		printf("> ");
-		i = 0;
-		char c;
 		fgets(input, sizeof(input), stdin);
-		printf("%s", input);
+		printf("(%s)\n", input);
+		if (strcmp(input,"exit\n") == 0){
+			return;
+		}
 	}
 }
+
