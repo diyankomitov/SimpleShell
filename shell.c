8941c3ebd549ae560ff9be3c32a7c63f447e399f
Diyan Komitov <diyan.komitov.2015@uni.strath.ac.uk>
2017-02-02 14:29:26 +0000

    added parsing functionality, fixed fgets adding newline char to input

--- shell.c
+++ shell.c
@@ -4,20 +4,43 @@
 #define STR_LEN 512
 
 void read_from_console();
+void parse(char *input);
+
 void main(int argc, char * argv[]){
 	read_from_console();
 	return;	
 }
 
+/*
+ * Reads user input from the console
+ */
 void read_from_console(){
 	char input[STR_LEN] = {""};
 	while(1){
 		printf("> ");
 		fgets(input, sizeof(input), stdin);
-		printf("(%s)\n", input);
-		if (strcmp(input,"exit\n") == 0){
+		input[strlen(input)-1] = '\0';
+
+		parse(input);
+
+		if (strcmp(input,"exit") == 0){
 			return;
 		}
 	}
 }
 
+/*
+ * Parses an input string, splits it
+ * into smaller tokens and prints them out.
+ * Parameteres:
+ *		input: the input string to be parsed.
+ */
+void parse(char *input){
+	char *token = strtok(input, " ");
+	while (token != NULL)
+	{
+		printf("'%s'\n", token);
+
+		token = strtok(NULL, " ");
+	}
+}
