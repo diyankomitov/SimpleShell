5c600dc37ddc001bdf7a8c26cf0b5d16b085632d
Bence Sebetsyen <bence.sebestye.2015@uni.strath.ac.uk>
2017-02-03 14:23:46 +0000

    header files created

--- shell.c
+++ shell.c
@@ -1,68 +1,6 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#define STR_LEN 512
-
-void read_from_console();
-void parse(char *input);
-void flush_stdin();
+#include <shell_input.h>
 
 void main(int argc, char * argv[]){
 	read_from_console();
 }
 
-/*
- * Reads user input from the console
- */
-void read_from_console() {
-	char input[STR_LEN+1]; 		// +1 to hold '\0' character
-	while(1){
-		printf("> ");
-		fgets(input, STR_LEN+2, stdin);		// +2 since fgets adds an extra '\n' char which we remove later
-
-		
-		if (feof(stdin)) {		// if Ctrl+D which gives EOF, exit
-				printf("\n");
-				return;
-		}
-		else if (input[strlen(input)-1] != '\n')		// if input is too long it won't end in a '\n'
-		{
-			flush_stdin();
-			printf("> Error: input is too long\n");
-		}
-		else {
-			input[strlen(input)-1] = '\0';		// otherwise replace '\n' with '\0'
-
-			if (strcmp(input,"exit") == 0) {
-				return;
-			}
-
-			parse(input);
-		}
-	}
-}
-
-/*
- * Parses an input string, splits it
- * into smaller tokens and prints them out.
- * Parameteres:
- *		input: the input string to be parsed.
- */
-void parse(char *input){
-	char *token = strtok(input, " ");		// currently only splits on space
-	while (token != NULL)
-	{
-		printf("'%s'\n", token);
-
-		token = strtok(NULL, " ");
-	}
-}
-
-/*
- * Clears stdin
- */
-void flush_stdin() {
-	int ch;
-	while ((ch = fgetc(stdin)) != EOF && ch != '\n') {}
-		//this will read and remove one character at a time from stdin until it reaches an EOF or a new line
-}
---
+++ shell_input.h
@@ -0,0 +1,8 @@
+#ifndef SHELL_INPUT_HEADER
+#define SHELL_INPUT_HEADER
+
+void read_from_console();
+void parse(char *input);
+void flush_stdin();
+
+#endif
