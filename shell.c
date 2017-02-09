6e2c6659b393ac7614d45ecf913c3ba1ad5c6d03
Bence Sebestyen <bence.sebestyen.2015@uni.strath.ac.uk>
2017-02-09 16:00:42 +0000

    some performance changes

--- shell.c
+++ shell.c
@@ -9,11 +9,12 @@ void main(int argc, char * argv[])
 {
 	char input[INPUT_LEN+1]; 		// +1 to hold '\0' character
 
+	char* tokens[INPUT_LEN/2] = { NULL };		// declared every loop so that it is cleared from tokens
 	while(1){
 		printf("> ");
 		fgets(input, INPUT_LEN+2, stdin);		// +2 since fgets adds an extra '\n' char which we remove later
 
-		char* tokens[50] = { NULL };		// declared every loop so that it is cleared from tokens
+		memset(tokens, 0, (INPUT_LEN/2));
 		parse(tokens, input);
 
 		echo_input(tokens);			// test, remove later
