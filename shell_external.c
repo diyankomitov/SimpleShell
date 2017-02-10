7af516ddf5ac84bad747377cede56f279c8d50f5
Bence Sebestyen <bence.sebestyen.2015@uni.strath.ac.uk>
2017-02-10 14:53:20 +0000

    catching error numbers

--- shell_external.c
+++ shell_external.c
@@ -3,20 +3,31 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <stdlib.h>
+#include <errno.h>
 
 uint8_t exec_external(char** tokenized_command)
 {
     __pid_t child_process_id = fork();
 
+    if (child_process_id < 0)
+        return 1;
+
     if (child_process_id == 0)
     {
         execvp(tokenized_command[0], tokenized_command);
+
+        exit(errno);
     }
     else
     {
-        // wait for the fork execution finish
-        wait(NULL);
+        int status;
+        wait(&status);
+
+        // error handling based on the reference
+        // http://man7.org/linux/man-pages/man3/errno.3.html
+        printf("error number:%d\n", status);
     }
 
-    return 1;
+    return 0;
 }
