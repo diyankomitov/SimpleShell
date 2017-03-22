524fa624caab350c259859caf00ee0c33364556e
Karol Groszewski <karol.groszewski.2015@uni.strath.ac.uk>
2017-03-22 15:36:36 +0000

    adding alias

--- shell_alias.c
+++ shell_alias.c
@@ -4,13 +4,16 @@
 
 #include <shell_alias.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <shell_commands.h>
 
 #define ALIAS_LEN 10
 
 typedef struct
 {
     char* name;
-    char* command[HIST_LEN][INPUT_LEN/2];
+    char* command[INPUT_LEN/2];
 } aliascontainer;
 
 aliascontainer* aliases[ALIAS_LEN];
@@ -23,5 +26,27 @@ uint8_t count_aliases()
         count++;
 
     return count;
+}
+
+void add_alias(char* alias_name, char** command)
+{
+    uint8_t index = count_aliases();
+    aliascontainer* temp_alias = malloc(sizeof(aliascontainer));
+    if (index != 10)
+    {
+        temp_alias->name = strdup(alias_name);
+        uint8_t i = 0;
+        for (; command[i] != NULL; i++)
+            temp_alias->command[i] = strdup(command[i]);
+
+        temp_alias->command[i] = NULL;
+
+        aliases[index] = temp_alias;
+    }
+    else
+    {
+        printf("Error: The aliasaes is full!\n");
+        return;
+    }
 }
 
