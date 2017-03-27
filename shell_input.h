c370491b55b6148452f7230549807f18bfc2838c
Gordon Clark <ntb15144@uni.strath.ac.uk>
2017-03-27 15:05:20 +0100

    comments added for input

--- shell_input.h
+++ shell_input.h
@@ -4,18 +4,27 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+ 
 /*
- * Parses an input string, splits it into smaller tokens
- * and adds them to an array of char pointers
+ * Function takes input string and tokenises it into individual segments based on certain delimeters. 
  * Parameters:
- *		input: the input string to be parsed.
- *		output: the array of char pointers
+ * String Input (String)
+ * Tokenised Output (Array of Strings)
  */
-
 void parse(char **output, char *input);
 
+/*
+ * Function takes in tokenised input and displays it separated out
+ * Parameters:
+ * Tokenised input (Array of Strings)
+ */
 void echo_input(char **input);
 
+/*
+ * Function takes in an tokenised input strings and determines whether or not they are a history command
+ * Parameters:
+ * Tokenised input (Array of Strings)
+ */
 bool is_history_command(char** command);
 
 #endif
